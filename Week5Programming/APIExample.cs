057a251376e039af6c8a29bc43a12485ff8ca664
bran12v <brandon.a.vanek@gmail.com>
2024-08-13 14:49:20 -0400

    Week 5 programming

---
+++ Week5Programming/APIExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json; // json parser
+using System.Net.Http;
+using System.Net.Http.Headers; // httpclient
+
+namespace Week5Programming
+{
+    public static class APIExample
+    {
+        /*
+         * GET, query strings, parameters, etc
+         * OPTIONS
+         */
+        public static async Task<string> APIExampleRunAsync()
+        {
+
+            string URI = "https://pokeapi.co/api/v2/"; 
+            string parameters = "pokemon"; // ?limit=100000&offset=0
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URI);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage res = await client.GetAsync(parameters);
+
+            if (res.IsSuccessStatusCode)
+            {
+                var jsonString = await res.Content.ReadAsStringAsync(); // get the JSON string of the request body
+                JsonDocument json = JsonDocument.Parse(jsonString);
+                var jsonArray = json.RootElement.GetProperty("results");
+
+                for (int i = 0; i < jsonArray.GetArrayLength(); i++)
+                {
+                    Pokemon poke = jsonArray[i].Deserialize<Pokemon>()!;
+                    Console.WriteLine(poke.name);
+                }
+
+                //Dictionary<string, string> pokemondict = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString)!;
+
+                //Console.WriteLine(pokemonDict.ToString());
+                return jsonString;
+            }
+            else
+            {
+                Console.WriteLine("There was an issue with your request: Status code: " + res.StatusCode);
+                return "";
+            }
+        }
+    }
+}
---
+++ Week5Programming/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Week5Programming
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Program prog = new Program();
+            // Sync
+            //Coffee cup = prog.pourCoffee();
+            //Console.WriteLine("Your coffee is ready");
+            //Eggs eggs = prog.fryEggs();
+            //Console.WriteLine("Your eggs are ready");
+            //Bacon bacon = prog.fryBacon();
+            //Console.WriteLine("Your bacon is ready");
+            //Toast toast = prog.toastBread();
+            //Console.WriteLine("Your toast is ready");
+            //Juice juice = prog.pourJuice();
+            //Console.WriteLine("Your Juice is ready");
+
+            // Async
+            //Coffee cup = prog.pourCoffee();
+            //Console.WriteLine("Coffee's done.");
+            //var eggsTask = prog.FryEggsAsync(1);
+            //var baconTask = prog.FryBaconAsync(10);
+            //var toastTask = prog.MakeToastAsync(2);
+
+            //var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+
+            //while (breakfastTasks.Count > 0)
+            //{
+            //    Task finishedTask = await Task.WhenAny(breakfastTasks); // return task that finishes first
+            //    if (finishedTask == eggsTask)
+            //    {
+            //        Console.WriteLine("The eggs are ready");
+            //    }
+            //    else if (finishedTask == baconTask)
+            //    {
+            //        Console.WriteLine("The bacon is ready");
+            //    }
+            //    else if (finishedTask == toastTask)
+            //    {
+            //        Console.WriteLine("The toast is ready");
+            //    }
+            //    breakfastTasks.Remove(finishedTask);
+            //}
+
+            await APIExample.APIExampleRunAsync();
+        }
+
+        private Coffee pourCoffee()
+        {
+            Console.WriteLine("Pouring your coffee");
+            return new Coffee(); 
+        }
+
+        private Eggs fryEggs()
+        {
+            Console.WriteLine("Your eggs are frying");
+            return new Eggs();
+        }
+
+        private Bacon fryBacon()
+        {
+            Console.WriteLine("Your bacon is frying");
+            return new Bacon();
+        }
+
+        private Toast toastBread()
+        {
+            Console.WriteLine("Toasting your bread");
+            return new Toast();
+        }
+
+        private Juice pourJuice()
+        {
+            Console.WriteLine("Pouring your juice");
+            return new Juice();
+        }
+
+        //two very important keywords
+        // async and await - IMPORTANT
+        // async is applied to a method
+        // await is applied to an action in a method
+        // Task - similar to a thread, actions you want to run async
+
+        private async Task<Eggs> FryEggsAsync(int numOfEggs)
+        {
+            Console.WriteLine("Heating the pan...");
+            await Task.Delay(1000); // wait 1000 ms
+            Console.WriteLine($"Cracking {numOfEggs} into pan...");
+            Console.WriteLine("Frying eggs...");
+            await Task.Delay(3000);
+            Console.WriteLine("Putting eggs on a plate for eating!");
+            return new Eggs();
+        }
+
+        private async Task<Bacon> FryBaconAsync(int slices)
+        {
+            Console.WriteLine("Warming the pan...");
+            await Task.Delay(200);
+            Console.WriteLine("Cooking first side of bacon...");
+            await Task.Delay(4000);
+            for (int i = 0; i < slices; i++)
+            {
+                Console.WriteLine("Flipping a slice of bacon");
+            }
+            Console.WriteLine("Cooking second side of bacon...");
+            await Task.Delay(4000);
+            Console.WriteLine("Putting bacon on a plate for eating!");
+            return new Bacon();
+        }
+
+        private async Task<Toast> MakeToastAsync(int slices)
+        {
+            for (int i = 0; i < slices; i++)
+            {
+                Console.WriteLine("Putting slice of toast into the toaster");
+            }
+            Console.WriteLine("Start toasting");
+            await Task.Delay(6000);
+            Console.WriteLine("Removing toast from toaster");
+            return new Toast();
+        }
+
+
+        private class Coffee {}
+        private class Juice { }
+        private class Bacon { }
+        private class Toast { }
+        private class Eggs { }
+    }
+}
